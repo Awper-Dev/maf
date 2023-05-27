41ca6e15ee43a78ad78b22b5be6e001aa93733e3
awper <awperdev@gmail.com>
2023-05-27 19:22:15 +0200

    push benchmark

--- code/jvm/src/main/scala/maf/cli/runnables/InterpretProgram.scala
+++ code/jvm/src/main/scala/maf/cli/runnables/InterpretProgram.scala
@@ -1,5 +1,6 @@
 package maf.cli.runnables
 
+import maf.bench.scheme.SchemeBenchmarkPrograms
 import maf.language.CScheme.*
 import maf.language.change.CodeVersion.*
 import maf.language.scheme.interpreter.*
@@ -10,26 +11,52 @@ import maf.util.benchmarks.Timeout
 import scala.concurrent.duration.*
 
 object InterpretProgram extends App:
-  val text = Reader.loadFile("test/optimizations/constant-folding.scm")
-  val interpreter = new SchemeInterpreter((_, _) => ())
+  def benchmarkFile(str: String) =
+    //val text = Reader.loadFile("test/optimizations/constant-folding.scm")
+    val text = Reader.loadFile(str)
+    val interpreter = new SchemeInterpreter((_, _) => ())
 
-  var nums: List[Long] = List()
 
-  for (a <- 1 to 30) do
-    try
-      val start = System.currentTimeMillis()
-      val res = interpreter.run(
-        CSchemeParser.parseProgram(text),
-        Timeout.start(Duration(10, MINUTES)),
-        New
-      )
-      val end = System.currentTimeMillis()
+    var nums: List[Long] = List()
+    var optNums: List[Long] = List()
 
-      val diff: Long = end - start
-      println(a + ": " + diff)
-      if a >= 6 then
-        nums = diff :: nums
+    for (a <- 1 to 30) do
+      try
+        val start = System.currentTimeMillis()
+        val res = interpreter.run(
+          CSchemeParser.parseProgram(text),
+          Timeout.start(Duration(10, MINUTES)),
+          New
+        )
+        val end = System.currentTimeMillis()
+        val diff: Long = end - start
 
-    catch case ProgramError(msg) => System.err.nn.println(msg)
+        //println(a + ": " + diff)
+        if a >= 6 then
+          nums = diff :: nums
 
-  println(nums)
+      catch case ProgramError(msg) => System.err.nn.println(msg)
+
+    for (a <- 1 to 30) do
+      try
+        val newText = OptimizeProgram.fullyOptimize(text, true, 1, false)
+        val start2 = System.currentTimeMillis()
+        val res2 = interpreter.run(
+          CSchemeParser.parseProgram(newText),
+          Timeout.start(Duration(10, MINUTES)),
+          New
+        )
+        val end2 = System.currentTimeMillis()
+        val diff2: Long = end2 - start2
+
+        //println(a + ": " + diff)
+        if a >= 6 then
+          optNums = diff2 :: optNums
+
+      catch case ProgramError(msg) => System.err.nn.println(msg)
+
+    println("----------")
+    println(str)
+    println("regular: " + nums)
+    println("optimized: " + nums)
+  SchemeBenchmarkPrograms.scp1Working.foreach((program: String) => benchmarkFile(program))
